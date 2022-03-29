84172bde73837cdee021c183191480806973eb67
Rakic_Jugo <rakic.nemanja@hak-liezen.at>
2022-03-29 15:52:35 +0200

    Projektdateien hinzufügen.

---
+++ Foederkurs_List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace Foederkurs_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Colllections
+            ArrayList normaleListe = new ArrayList();
+            normaleListe.Add(1);
+            normaleListe.Add("Karli");
+            normaleListe.Add(true);
+
+            foreach (var item in normaleListe)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<string> listOfStrings = new List<string>();
+
+
+        }
+    }
+}
